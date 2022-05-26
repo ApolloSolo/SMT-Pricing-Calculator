bd04e52322b83e7aa4cc59c0bda3366a907fbc09
ApolloSolo <matt1teixeira@gmail.com>
2022-05-26 19:31:51 -0400

    Calculate savings and smash costs

--- calcPrice.js
+++ calcPrice.js
@@ -1,5 +1,5 @@
 const calcCurrent = document.getElementById("calc-current");
-const currentState = document.getElementById("currentState");
+const container = document.getElementById("container");
 
 const c = (el) => {
   console.log(el);
@@ -36,19 +36,34 @@ const calcCurrentData = () => {
   currentAnualHaulCost.textContent = (currentMonthHaulCostValue * 12).toFixed(1)
 
   //Remove hide class
-  currentState.classList.remove("hide");
+  container.classList.remove("hide");
+  calcCurrent.textContent = "Update"
 
   // Value Outputs
   const compactionRatioValue = parseFloat(document.getElementById("compRate").value.trim())/100;
   const futureMonthHaulsEl = document.getElementById("futureMonthHauls")
-  let futureMonthHaulsValue = ((1 - compactionRatioValue) * currentMonthHaulsValue).toFixed(1)
+  let futureMonthHaulsValue = ((1 - compactionRatioValue) * currentMonthHaulsValue).toFixed(0)
   futureMonthHaulsEl.textContent = futureMonthHaulsValue
 
   const eliminatedHauls = document.getElementById('eliminatedHauls');
   let eliminatedHaulsValue = (currentMonthHaulsValue - futureMonthHaulsValue).toFixed(1)
   c(eliminatedHaulsValue);
-  eliminatedHauls.textContent = eliminatedHaulsValue
-};
+  eliminatedHauls.textContent = eliminatedHaulsValue;
 
+  // New Haul cost
+  const newMonthlyHaulCostEl = document.getElementById('newMonthlyHaulCost');
+  let newMonthlyHaulCostValue = (futureMonthHaulsValue * currentPerHaulCost).toFixed(0);
+  newMonthlyHaulCostEl.textContent = newMonthlyHaulCostValue
+
+  let expectedDiscountPrice = currentMonthHaulCostValue*(compactionRatioValue);
+
+  let smashFeeMonthValue = expectedDiscountPrice - newMonthlyHaulCostValue
+  c(smashFeeMonthValue);
+  const smashFeeEl = document.getElementById("smashFee").textContent = smashFeeMonthValue;
+
+  const totalMonthlyEl = document.getElementById("totalMonthly").textContent = expectedDiscountPrice.toFixed(2);
+
+  const totalAnnualEl = document.getElementById("totalAnnual").textContent = (expectedDiscountPrice * 12).toFixed(2);
+  }
 
 calcCurrent.addEventListener('click', calcCurrentData);
